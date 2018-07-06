2cc0f19cacb3ecc18028f765ab4a55cd21467922
Steve Chan <chanshunli@gmail.com>
2018-07-06 12:42:46 +0800

    alias cljc='  javac -cp /Users/stevechan/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar  ' alias clje='  java -cp /Users/stevechan/.m2/repository/org/clojure/clojure/1.8.0/clojure-1.8.0.jar  '
    ➜  meghanada-server git:(master) ✗ clje foo
    Error: Could not find or load main class foo
    ➜  meghanada-server git:(master) ✗

---
+++ Foo.java
@@ -0,0 +1,19 @@
+// Foo.java
+
+import clojure.lang.RT;
+import clojure.lang.Var;
+
+public class Foo {
+    public static void main(String[] args) throws Exception {
+        // Load the Clojure script -- as a side effect this initializes the runtime.
+        RT.loadResourceScript("foo.clj");
+
+        // Get a reference to the foo function.
+        Var foo = RT.var("user", "foo");
+
+        // Call it!
+        Object result = foo.invoke("Hi", "there");
+        System.out.println(result);
+    }
+}
+
