24e001f95d9faad9fc310c3ef5b2d1b01a13a923
Steve Chan <chanshunli@gmail.com>
2018-07-06 15:19:23 +0800

    任何开发都要用上LISP的速度,都是无敌的

--- Foo.java
+++ Foo.java
@@ -2,6 +2,7 @@
 
 import clojure.lang.RT;
 import clojure.lang.Var;
+import clojure.lang.IFn;
 
 public class Foo {
     public static void main(String[] args) throws Exception {
@@ -14,6 +15,17 @@ public class Foo {
         // Call it!
         Object result = foo.invoke("Hi", "there");
         System.out.println(result);
+
+        ///
+        //RT.var("clojure.core","eval").invoke(RT.var("clojure.core","read-string").invoke("(ns cljuser) (defonce this-atom (atom nil)) #_(defn main [this] (reset! this-atom this))  (defn abc [a] (prn a)) "));
+        // IFn fn = RT.var("cljuser","main");
+        //fn.invoke(1111);
+        // IFn fn = RT.var("cljuser","abc");
+        //fn.invoke(1111);        
+        //Exception in thread "main" java.lang.IllegalStateException: Can't change/establish root binding of: *ns* with set
+        //          at clojure.lang.Var.set(Var.java:221)
+        //          at clojure.lang.RT$1.invoke(RT.java:241)
+        
     }
 }
 
